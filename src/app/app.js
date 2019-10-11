9b2101671cf4fa6e3674b55f0fa264ef2be3f9e2
Guillaume <guillaume.barreau@zenika.com>
2019-10-11 09:58:20 +0200

    feat(Added): "eslint configurations"

--- src/app/app.js
+++ src/app/app.js
@@ -3,11 +3,11 @@ import './app.css';
 import { Layout } from "./layout/layout";
 
 function App() {
-  return (
-    <Layout>
-      <section></section>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <section></section>
+        </Layout>
+    );
 }
 
 export default App;
