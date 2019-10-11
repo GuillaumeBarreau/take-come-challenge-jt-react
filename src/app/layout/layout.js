6c7f602901124639c446fd63626fd1d057be5104
Guillaume <guillaume.barreau@zenika.com>
2019-10-11 06:14:53 +0200

    feat(Added): "files configurations"

---
+++ src/app/layout/layout.js
@@ -0,0 +1,17 @@
+import "./layout.css";
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Layout = ({ children }) => {
+  return (
+    <>
+      <main className="main">
+        {children}
+      </main>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
